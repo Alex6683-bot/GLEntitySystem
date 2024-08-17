1c9f46aa57f0110f2f17a9b65488ada1ef861c24
Alex <alexsmailfolder@gmail.com>
2024-08-17 15:20:18 +1000

    Update to IComponent.cs

--- ECS/IComponent.cs
+++ ECS/IComponent.cs
@@ -3,16 +3,11 @@
     public interface IComponent
     {
         public Entity Entity { get; set; }
-        public bool enabled { get; set; }
         public void OnStart(Entity entity)
         { 
             Entity = entity;
-            enabled = true;
         }
-        public void OnUpdate(Entity entity)
-        {
-            if (!enabled) return;
-        }
+        public void OnUpdate(Entity entity);
 
     }
 }
